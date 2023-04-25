7316369e46d3e135f2c0e83d605b4c6b3aabb45c
Federico Degiovanni <federicoadegiovanni@gmail.com>
2023-04-25 20:36:22 -0300

    add basics

--- assets/scenes/Game.js
+++ assets/scenes/Game.js
@@ -24,22 +24,63 @@ export default class Game extends Phaser.Scene {
 
     // add shapes group
     this.shapesGroup = this.physics.add.group();
-    this.shapesGroup.create(100, 0, 'diamond');
-    this.shapesGroup.create(200, 0, 'triangle');
-    this.shapesGroup.create(300, 0, 'square');
+    // this.shapesGroup.create(100, 0, 'diamond');
+    // this.shapesGroup.create(200, 0, 'triangle');
+    // this.shapesGroup.create(300, 0, 'square');
+    // create event to add shapes
+    this.time.addEvent({
+      delay: 1500,
+      callback: this.addShape,
+      callbackScope: this,
+      loop: true,
+    });
     
     // add sprite player
     this.player = this.physics.add.sprite(100, 450, "ninja");
     this.player.setCollideWorldBounds(true);
+    
     // create cursors
+    this.cursors = this.input.keyboard.createCursorKeys();
+    
     // add collider between player and platforms
     // add collider between player and shapes
     // add overlap between player and shapes
+    this.physics.add.collider(this.player, platforms);
+    this.physics.add.collider(this.player, this.shapesGroup);
+    this.physics.add.collider(platforms, this.shapesGroup);
+
+    
   }
 
   update() {
     // check if not game over or win
     // update player movement
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-250);
+    } else {
+      if (this.cursors.right.isDown) {
+        this.player.setVelocityX(250);
+      } else {
+        this.player.setVelocityX(0);
+      }
+    }
+
     // update player jump
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+  }
+
+  addShape() {
+    // get random shape
+    const randomShape = Phaser.Math.RND.pick(['diamond', 'square', 'triangle']);
+
+    // get random position x
+    const randomX = Phaser.Math.RND.between(0, 800);
+
+    // add shape to screen
+    this.shapesGroup.create(randomX, 0, randomShape);
+
+    console.log("shape is added", randomX, randomShape);
   }
 }
